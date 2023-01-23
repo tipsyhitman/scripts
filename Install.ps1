afc0539cff2f81725b88c29c77765ba4856d5033
tipsyhitman <76920402+tipsyhitman@users.noreply.github.com>
2023-01-23 13:24:50 -0600

    Create Install.ps1

---
+++ Install.ps1
@@ -0,0 +1,6 @@
+winget install "Discord" -h
+winget install "Mozilla.Firefox" -h
+winget install "Valve.Steam" -h
+winget install "7zip.7zip" -h
+winget install "Notepad++" -h
+winget install "VideoLAN.VLC" -h
